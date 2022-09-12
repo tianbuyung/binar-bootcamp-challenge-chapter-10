10ea140487c3d9bfb6dcd86891dbef731e1c5c9f
akurnia1 <akurnia@binaracademy.org>
2022-09-12 23:22:33 +0700

    boilerplate server

---
+++ server/controllers/UserController.js
@@ -0,0 +1,13 @@
+const getUser = (req, res) => {
+  res.json({
+    message: "Get User",
+  });
+};
+
+const getUserAllUser = (req, res) => {
+  res.json({
+    message: "Get User",
+  });
+};
+
+module.exports = { getUser, getUserAllUser };
