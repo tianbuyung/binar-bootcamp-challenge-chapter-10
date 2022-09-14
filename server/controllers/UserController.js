b04376c4c970fcf94051063ea4b11736190f6b2f
florensius daniel josef <capricondaniel@gmail.com>
2022-09-14 21:08:46 +0700

    error : type is not a constructor

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,4 @@
-const Model = require("../models");
-const { User } = Model;
+const { User } = require("../models");
 
 const getUser = (req, res) => {};
 
@@ -7,21 +6,21 @@ const createUser = (req, res) => {
 	const { username, password, email } = req.body;
 	console.log("cek data masuk : ", username, password, email);
 
-	User.create({
-		username: username,
-		password: password,
-		email: email,
-	})
-		.then(() => {
-			res.status(200).json({
-				message: "Successfully create user",
-			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				message: "error creating user : " + err.message,
-			});
-		});
+	// User.create({
+	// 	username: username,
+	// 	password: password,
+	// 	email: email,
+	// })
+	// 	.then(() => {
+	// 		res.status(200).json({
+	// 			message: "Successfully create user",
+	// 		});
+	// 	})
+	// 	.catch((err) => {
+	// 		res.status(500).json({
+	// 			message: "error creating user : " + err.message,
+	// 		});
+	// });
 };
 
 const getUserAllUser = (req, res) => {
--- server/models/order.js
+++ server/models/order.js
@@ -1,44 +1,46 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.Order.belongsTo(models.Cart);
-      models.Order.hasMany(models.OrderDetail, {
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      });
-    }
-  }
-  Order.init({
-    cartId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        isInt: true,
-        notNull: true,
-        notEmpty: true
-      }
-    },
-    totalOrder: {
-      type: DataTypes.NUMERIC,
-      validate: {
-        isDecimal: true,
-        notNull: true,
-        notEmpty: true
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
-  return Order;
+	class Order extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			models.Order.belongsTo(models.Cart);
+			models.Order.hasMany(models.OrderDetail, {
+				onDelete: "CASCADE",
+				onUpdate: "CASCADE",
+			});
+		}
+	}
+	Order.init(
+		{
+			cartId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					isInt: true,
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+			totalOrder: {
+				type: DataTypes.NUMERIC,
+				allowNull: false,
+				validate: {
+					isDecimal: true,
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "Order",
+		}
+	);
+	return Order;
 };
