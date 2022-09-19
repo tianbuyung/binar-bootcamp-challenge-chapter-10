5f94a10edeeebdcefbef71a07ea021c02cc52b06
akurnia1 <akurnia@binaracademy.org>
2022-09-19 20:18:41 +0700

    change services

---
+++ client/src/services/ProductService.js
@@ -0,0 +1,22 @@
+import BaseService from "./BaseService";
+
+export default class ProductService extends BaseService {
+  addProduct = async (data) => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(data),
+    };
+
+    console.log("options", options);
+
+    return await this.fetch("/admin/products", options, true);
+  };
+
+  getAllProducts = async (query) => {
+    const options = {
+      method: "GET",
+    };
+
+    return await this.fetch("/admin/products" + query, options, true);
+  };
+}
