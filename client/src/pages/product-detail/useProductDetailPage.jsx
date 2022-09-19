bc83b6420caef9819062ec694b87015202bdaee4
akurnia1 <akurnia@binaracademy.org>
2022-09-19 20:18:41 +0700

    change services

---
+++ client/src/pages/product-detail/useProductDetailPage.jsx
@@ -0,0 +1,5 @@
+const useProductDetailPage = () => {
+
+}
+
+export default useProductDetailPage;
---
+++ client/src/services/AuthService.js
@@ -0,0 +1,7 @@
+import BaseService from "./BaseService";
+
+export default class AuthService extends BaseService {
+  login = async () => {};
+
+  register = async () => {};
+}
