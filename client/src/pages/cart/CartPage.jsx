4df39f8f73471e85bdd82f08a0800754ae303493
rikyyang <yang.riky@gmail.com>
2022-09-22 20:25:10 +0700

    Riky - create & update api integration

--- client/src/pages/cart/CartPage.jsx
+++ client/src/pages/cart/CartPage.jsx
@@ -4,6 +4,19 @@ import { useEffect } from "react";
 const CartPage = () => {
 	const [loading, setLoading] = useState(true);
 	const [cart, setCart] = useState();
+	const [inputs, setInputs] = useState({});
+
+	const handleChange = (event, productId, productName) => {
+		const id = event.target.id;
+		const value = event.target.value;
+		setInputs(values => ({ ...values, [id]: value }));
+
+		if (value > 0) {
+			updateCartDetail(event, productId);
+		} else {
+			deleteCartDetail(id, productName);
+		}
+	}
 
 	useEffect(() => {
 		fetchCart();
@@ -25,12 +38,28 @@ const CartPage = () => {
 		}
 	}
 
-	// update
+	const updateCartDetail = async (event, productId) => {
+		const response = await fetch(
+			process.env.REACT_APP_SERVER + "/cartDetails/", {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+			body: JSON.stringify({
+				ProductId: productId,
+				qty: event.target.value
+			})
+		});
+
+		if (!response.ok) {
+			alert(`HTTP error! status: ${response.status}`);
+		}
+	}
 
 	const deleteCartDetail = async (id, cartProductName) => {
 		if (window.confirm("Are you sure you want to delete this item '" + cartProductName + "'?")) {
 			const response = await fetch(
-				process.env.REACT_APP_SERVER + "/carts/" + id, { method: 'DELETE' }
+				process.env.REACT_APP_SERVER + "/cartDetails/" + id, { method: 'DELETE' }
 			);
 
 			if (!response.ok) {
@@ -40,6 +69,8 @@ const CartPage = () => {
 				alert(data.message);
 				fetchCart();
 			}
+		} else {
+			// return before value
 		}
 	}
 
@@ -49,7 +80,16 @@ const CartPage = () => {
 			{
 				!loading ? (
 					<ul>
-						{cart.CartDetails.map((cartDetail) => <li key={cartDetail.id}>{cartDetail.Product.name} <button className="btn btn-outline-secondary" type="button" onClick={() => deleteCartDetail(cartDetail.id, cartDetail.Product.name)}><i className="bi-trash"></i></button></li>)}
+						{cart.CartDetails.map((cartDetail) => <li key={cartDetail.id}>
+							{cartDetail.Product.name}
+							<input type="number" className="form-control" placeholder="Qty" min="1"
+								id={'qty-' + cartDetail.id} value={inputs['qty-' + cartDetail.id] || cartDetail.qty}
+								onChange={(event) => handleChange(event, cartDetail.ProductId, cartDetail.Product.name)} />
+							<button className="btn btn-danger" type="button"
+								onClick={() => deleteCartDetail(cartDetail.id, cartDetail.Product.name)}>
+								<i className="bi-trash"></i>
+							</button>
+						</li>)}
 					</ul>
 				) : (
 					<div>
