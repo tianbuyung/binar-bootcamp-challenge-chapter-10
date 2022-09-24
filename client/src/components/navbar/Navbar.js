97bcb456a50c9dbeceb1499ba745a5d85098511e
Septian Maulana <septianm028@gmail.com>
2022-09-24 22:56:15 +0700

    [tianbuyung] - finalized homepage with link to product list and detail product also edit Navbar for sticky-top

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -3,37 +3,35 @@ import Navbar from "react-bootstrap/Navbar";
 import useAuth from "../../hooks/useAuth";
 
 const NavbarComponent = ({ variant, bg = "light" }) => {
-	const { isLogin } = useAuth();
-	return (
-		<Navbar variant={variant} bg={bg} expand="lg">
-			<Container>
-				<Navbar.Brand href="#home">Home</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-				{isLogin() ? (
-					<>
-						<Navbar.Brand>
-							<div className="cursor-pointer">Profile</div>
-						</Navbar.Brand>
-						<Navbar.Brand>
-							<div className="cursor-pointer">Logout</div>
-						</Navbar.Brand>
-					</>
-				) : (
-					<>
-						<Navbar.Brand>
-							<div className="cursor-pointer">Login</div>
-						</Navbar.Brand>
-						<Navbar.Brand>
-							<div className="cursor-pointer">
-								Register
-							</div>
-						</Navbar.Brand>
-					</>
-				)}
-			</Container>
-		</Navbar>
-	);
+  const { isLogin } = useAuth();
+  return (
+    <Navbar variant={variant} bg={bg} expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+        {isLogin() ? (
+          <>
+            <Navbar.Brand>
+              <div className="cursor-pointer">Profile</div>
+            </Navbar.Brand>
+            <Navbar.Brand>
+              <div className="cursor-pointer">Logout</div>
+            </Navbar.Brand>
+          </>
+        ) : (
+          <>
+            <Navbar.Brand>
+              <div className="cursor-pointer">Login</div>
+            </Navbar.Brand>
+            <Navbar.Brand>
+              <div className="cursor-pointer">Register</div>
+            </Navbar.Brand>
+          </>
+        )}
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavbarComponent;
--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,17 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar";
 import CategoryService from "../../services/CategoryService";
 
 const categoryService = new CategoryService();
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(true);
   const [getCategory, setGetCategory] = useState([]);
 
   const fetchGetCategoryHandler = useCallback(async () => {
     try {
       const data = await categoryService.getAllCategories();
       setGetCategory(data.categories);
+      setLoading(false);
     } catch (error) {
       // silent e
     }
@@ -25,33 +28,63 @@ const HomePage = () => {
     <div>
       <Navbar />
       <Container>
-        <h2 className="text-start h2">Jelajahi Produk Kami</h2>
-        <ul>
-          {getCategory.map((category) => {
-            return (
-              <div key={category.id}>
-                <h1>{category.name}</h1>
-                <Row xs={1} md={5} className="g-4">
-                  {category.Products.slice(0, 5).map((product) => (
-                    <Col key={product.id}>
-                      <Card style={{ height: "400px" }}>
-                        <Card.Img
-                          variant="top"
-                          src="https://res.cloudinary.com/drqqwwpen/image/upload/v1596474380/pcs/not-available_g2vsum.jpg"
-                        />
-                        <Card.Body>
-                          <Card.Title>{product.name}</Card.Title>
-                          <Card.Text>Price: Rp. {product.price}K</Card.Text>
-                          <Button variant="success">Beli</Button>
-                        </Card.Body>
-                      </Card>
+        {!loading ? (
+          <>
+            <h2 className="text-start h2 mt-3">Jelajahi Produk Kami</h2>
+            {getCategory.map((category) => {
+              return (
+                <div
+                  className="my-3 p-3 bg-secondary rounded"
+                  key={category.id}
+                >
+                  <Row className="align-items-center">
+                    <Col lg={2} xl={2} className="text-start text-white">
+                      <h3>{category.name}</h3>
                     </Col>
-                  ))}
-                </Row>
-              </div>
-            );
-          })}
-        </ul>
+                    <Col lg={"auto"} xl={"10"}>
+                      <Row>
+                        <Col className="text-end my-3">
+                          <Link
+                            className="text-white text-decoration-none"
+                            to={`productlist/${category.id}`}
+                          >
+                            Lihat Semua
+                          </Link>
+                        </Col>
+                      </Row>
+                      <Row xs={1} md={5} className="g-4">
+                        {category.Products.slice(0, 5).map((product) => (
+                          <Col key={product.id}>
+                            <Card style={{ height: "400px" }}>
+                              <Card.Img
+                                variant="top"
+                                src="https://res.cloudinary.com/drqqwwpen/image/upload/v1596474380/pcs/not-available_g2vsum.jpg"
+                              />
+                              <Card.Body>
+                                <Card.Title>{product.name}</Card.Title>
+                                <Card.Text>
+                                  Price: Rp. {product.price}K
+                                </Card.Text>
+                                <Link
+                                  to={`product/${product.id}`}
+                                  className="btn btn-success"
+                                >
+                                  Beli
+                                </Link>
+                              </Card.Body>
+                            </Card>
+                          </Col>
+                        ))}
+                      </Row>
+                    </Col>
+                  </Row>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </Container>
     </div>
   );
