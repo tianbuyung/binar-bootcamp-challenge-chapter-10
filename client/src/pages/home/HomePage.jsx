6b900153518f6a4a9159f128d76e1438f5ffba57
Septian Maulana <septianm028@gmail.com>
2022-09-25 13:39:46 +0700

    [tianbuyung] - integrated server to client in profile page

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -7,120 +7,91 @@ import CategoryService from "../../services/CategoryService";
 const categoryService = new CategoryService();
 
 const HomePage = () => {
-	const [loading, setLoading] = useState(true);
-	const [getCategory, setGetCategory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [getCategory, setGetCategory] = useState([]);
 
-	const fetchGetCategoryHandler = useCallback(async () => {
-		try {
-			const data = await categoryService.getAllCategories();
-			setGetCategory(data.categories);
-			setLoading(false);
-		} catch (error) {
-			// silent e
-		}
-	}, []);
+  const fetchGetCategoryHandler = useCallback(async () => {
+    try {
+      const data = await categoryService.getAllCategories();
+      setGetCategory(data.categories);
+      setLoading(false);
+    } catch (error) {
+      // silent e
+    }
+  }, []);
 
-	useEffect(() => {
-		fetchGetCategoryHandler();
-	}, [fetchGetCategoryHandler]);
+  useEffect(() => {
+    fetchGetCategoryHandler();
+  }, [fetchGetCategoryHandler]);
 
-	return (
-		<div>
-			<Navbar variant={"dark"} bg={"dark"} />
-			<Container>
-				{!loading ? (
-					<>
-						<h2 className="text-start h2 mt-3">
-							Jelajahi Produk Kami
-						</h2>
-						{getCategory?.map((category) => {
-							return (
-								<div
-									className="my-3 p-3 bg-secondary rounded"
-									key={category.id}
-								>
-									<Row className="align-items-center">
-										<Col
-											lg={2}
-											xl={2}
-											className="text-start text-white"
-										>
-											<h3>{category.name}</h3>
-										</Col>
-										<Col lg={"auto"} xl={"10"}>
-											<Row>
-												<Col className="text-end my-3">
-													<Link
-														className="text-white text-decoration-none"
-														to={`/product/category/${category.id}`}
-													>
-														Lihat
-														Semua
-													</Link>
-												</Col>
-											</Row>
-											<Row
-												xs={1}
-												md={5}
-												className="g-4"
-											>
-												{category?.Products?.slice(
-													0,
-													5
-												).map((product) => (
-													<Col
-														key={
-															product.id
-														}
-													>
-														<Link
-															to={`product/${product.id}`}
-															className="text-black text-decoration-none"
-														>
-															<Card
-																style={{
-																	height: "400px",
-																}}
-															>
-																<Card.Img
-																	variant="top"
-																	src="https://res.cloudinary.com/drqqwwpen/image/upload/v1596474380/pcs/not-available_g2vsum.jpg"
-																/>
-																<Card.Body>
-																	<Card.Title>
-																		{
-																			product.name
-																		}
-																	</Card.Title>
-																	<Card.Text>
-																		Price:
-																		Rp.{" "}
-																		{
-																			product.price
-																		}
-																		K
-																	</Card.Text>
-																	<Button variant="success">
-																		Buy
-																	</Button>
-																</Card.Body>
-															</Card>
-														</Link>
-													</Col>
-												))}
-											</Row>
-										</Col>
-									</Row>
-								</div>
-							);
-						})}
-					</>
-				) : (
-					<div>Loading...</div>
-				)}
-			</Container>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar variant={"dark"} bg={"dark"} />
+      <Container>
+        {!loading ? (
+          <>
+            <h2 className="text-start h2 mt-3">Jelajahi Produk Kami</h2>
+            {getCategory?.map((category) => {
+              return (
+                <div
+                  className="my-3 p-3 bg-secondary rounded"
+                  key={category.id}
+                >
+                  <Row className="align-items-center">
+                    <Col lg={2} xl={2} className="text-start text-white">
+                      <h3>{category.name}</h3>
+                    </Col>
+                    <Col lg={"auto"} xl={"10"}>
+                      <Row>
+                        <Col className="text-end my-3">
+                          <Link
+                            className="text-white text-decoration-none"
+                            to={`/product/category/${category.id}`}
+                          >
+                            Lihat Semua
+                          </Link>
+                        </Col>
+                      </Row>
+                      <Row xs={1} md={5} className="g-4">
+                        {category?.Products?.slice(0, 5).map((product) => (
+                          <Col key={product.id}>
+                            <Link
+                              to={`product/${product.id}`}
+                              className="text-black text-decoration-none"
+                            >
+                              <Card
+                                style={{
+                                  height: "400px",
+                                }}
+                              >
+                                <Card.Img
+                                  variant="top"
+                                  src="https://res.cloudinary.com/drqqwwpen/image/upload/v1596474380/pcs/not-available_g2vsum.jpg"
+                                />
+                                <Card.Body>
+                                  <Card.Title>{product.name}</Card.Title>
+                                  <Card.Text>
+                                    Price: Rp. {product.price}K
+                                  </Card.Text>
+                                  <Button variant="success">Details</Button>
+                                </Card.Body>
+                              </Card>
+                            </Link>
+                          </Col>
+                        ))}
+                      </Row>
+                    </Col>
+                  </Row>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </Container>
+    </div>
+  );
 };
 
 export default HomePage;
--- client/src/pages/profile/ProfilePage.jsx
+++ client/src/pages/profile/ProfilePage.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import {
-	Button,
-	Card,
-	Col,
-	Container,
-	ListGroup,
-	ListGroupItem,
-	Row,
+  Button,
+  Card,
+  Col,
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Row,
 } from "react-bootstrap";
 import Navbar from "../../components/navbar";
 import UserService from "../../services/UserService";
@@ -16,138 +16,103 @@ import "./ProfilePage.css";
 const userService = new UserService();
 
 const ProfilePage = () => {
-	const [profile, setProfile] = useState("");
-	const [email, setEmail] = useState("");
+  const [profile, setProfile] = useState("");
 
-	useEffect(() => {
-		const fetchGetUserHandler = async () => {
-			try {
-				const getEmail =
-					document.cookie || "Molly_Lebsack33@hotmail.com";
-				setEmail(getEmail);
-				const data = await userService.getUser(email);
-				setProfile(data);
-			} catch (error) {
-				//
-			}
-		};
-		fetchGetUserHandler(email);
-	}, [email]);
+  useEffect(() => {
+    const fetchGetUserHandler = async () => {
+      try {
+        const data = await userService.getUser();
+        setProfile(data);
+      } catch (error) {
+        //
+      }
+    };
+    fetchGetUserHandler();
+  }, []);
 
-	const socialMedias = [
-		{ name: "Website", icon: "bi bi-globe", data: "https://dummy.com" },
-		{ name: "Github", icon: "bi bi-github", data: "dummy" },
-		{ name: "Twitter", icon: "bi bi-twitter", data: "@dummy" },
-		{ name: "Instagram", icon: "bi bi-instagram", data: "dummy" },
-		{ name: "Facebook", icon: "bi bi-facebook", data: "dummy" },
-	];
+  const socialMedias = [
+    { name: "Website", icon: "bi bi-globe", data: "https://dummy.com" },
+    { name: "Github", icon: "bi bi-github", data: "dummy" },
+    { name: "Twitter", icon: "bi bi-twitter", data: "@dummy" },
+    { name: "Instagram", icon: "bi bi-instagram", data: "dummy" },
+    { name: "Facebook", icon: "bi bi-facebook", data: "dummy" },
+  ];
 
-	return (
-		<>
-			<Navbar variant={"dark"} bg={"dark"} />
-			<Container className="main-body">
-				<Row className="gutters-sm">
-					<Col md={4} className="mb-3">
-						<Card>
-							<Card.Body className="card-body">
-								<div className="d-flex flex-column align-items-center text-center">
-									<img
-										src="https://bootdey.com/img/Content/avatar/avatar7.png"
-										alt="Admin"
-										className="rounded-circle"
-										width="150"
-									/>
-									<div className="mt-3">
-										<h4>{profile.user?.name}</h4>
-										<p className="text-secondary mb-1">
-											{profile.user?.email}
-										</p>
-										<p className="text-muted font-size-sm">
-											Bay Area, San Francisco,
-											CA
-										</p>
-										<Button
-											variant="primary"
-											className="me-2"
-										>
-											Follow
-										</Button>
-										<Button variant="outline-primary">
-											Message
-										</Button>
-									</div>
-								</div>
-							</Card.Body>
-						</Card>
-						<Card className="mt-3">
-							<ListGroup className="list-group-flush">
-								{socialMedias.map(
-									(socialMedia, index) => {
-										return (
-											<ListGroupItem
-												className="d-flex justify-content-between align-items-center flex-wrap"
-												key={index}
-											>
-												<h6 className="mb-0">
-													<i
-														className={
-															socialMedia.icon
-														}
-													></i>{" "}
-													{
-														socialMedia.name
-													}
-												</h6>
-												<span className="text-secondary">
-													{
-														socialMedia.data
-													}
-												</span>
-											</ListGroupItem>
-										);
-									}
-								)}
-							</ListGroup>
-						</Card>
-					</Col>
-					<Col md={8}>
-						<Card className="mb-3">
-							<Card.Body>
-								<Row>
-									<Col sm={3}>
-										<h6 className="mb-0">
-											Full Name
-										</h6>
-									</Col>
-									<Col
-										sm={9}
-										className="text-secondary"
-									>
-										{profile.user?.name}
-									</Col>
-								</Row>
-								<hr />
-								<Row>
-									<Col sm={3}>
-										<h6 className="mb-0">
-											Email
-										</h6>
-									</Col>
-									<Col
-										sm={9}
-										className="text-secondary"
-									>
-										{profile.user?.email}
-									</Col>
-								</Row>
-								<hr />
-							</Card.Body>
-						</Card>
-					</Col>
-				</Row>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Navbar variant={"dark"} bg={"dark"} />
+      <Container className="main-body">
+        <Row className="gutters-sm">
+          <Col md={4} className="mb-3">
+            <Card>
+              <Card.Body className="card-body">
+                <div className="d-flex flex-column align-items-center text-center">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                    alt="Admin"
+                    className="rounded-circle"
+                    width="150"
+                  />
+                  <div className="mt-3">
+                    <h4>{profile.user?.name}</h4>
+                    <p className="text-secondary mb-1">{profile.user?.email}</p>
+                    <p className="text-muted font-size-sm">
+                      Bay Area, San Francisco, CA
+                    </p>
+                    <Button variant="primary" className="me-2">
+                      Follow
+                    </Button>
+                    <Button variant="outline-primary">Message</Button>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+            <Card className="mt-3">
+              <ListGroup className="list-group-flush">
+                {socialMedias.map((socialMedia, index) => {
+                  return (
+                    <ListGroupItem
+                      className="d-flex justify-content-between align-items-center flex-wrap"
+                      key={index}
+                    >
+                      <h6 className="mb-0">
+                        <i className={socialMedia.icon}></i> {socialMedia.name}
+                      </h6>
+                      <span className="text-secondary">{socialMedia.data}</span>
+                    </ListGroupItem>
+                  );
+                })}
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col md={8}>
+            <Card className="mb-3">
+              <Card.Body>
+                <Row>
+                  <Col sm={3}>
+                    <h6 className="mb-0">Full Name</h6>
+                  </Col>
+                  <Col sm={9} className="text-secondary">
+                    {profile.user?.name}
+                  </Col>
+                </Row>
+                <hr />
+                <Row>
+                  <Col sm={3}>
+                    <h6 className="mb-0">Email</h6>
+                  </Col>
+                  <Col sm={9} className="text-secondary">
+                    {profile.user?.email}
+                  </Col>
+                </Row>
+                <hr />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default ProfilePage;
