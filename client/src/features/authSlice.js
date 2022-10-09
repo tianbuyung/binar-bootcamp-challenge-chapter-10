8a6005313cda1cbbffe5bd083d1f75719e9c4c15
florensius daniel josef <capricondaniel@gmail.com>
2022-10-09 17:34:23 +0700

    harus pakai redux-thunk

--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -1,34 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, AsyncThunkAction, createAsyncThunk } from "@reduxjs/toolkit";
 
-import AuthService from "./services/AuthService";
+import AuthService from "../services/AuthService";
 
 const authservice = new AuthService();
+
+const cekUser = createAsyncThunk('users/status', async () => {
+	try {
+		const user = await authservice.verifyUser();
+		return user;
+	} catch (error) {
+		console.log("error whlie verify user : ", error.message);
+	}
+})
+
 const authSlice = createSlice({
 	name: "cekLogin",
 	initialState: {
-		isLogin: false,
+		// isLogin: false,
 		isAdmin: false,
 		isUser: false,
 	},
 	// ! coba dulu
 	reducers: {
-		cekUser: async (state) => {
-			const verify = await authservice.verifyUser();
-			if (verify.status === 200) {
-				state.isLogin = true;
-				state.isUser = true;
-			}
+		user: {
+			const verify = cekUser;
 		},
 		cekAdmin: async (state) => {
 			const verify = await authservice.verifyAdmin();
 			if (verify.status === 200) {
-				state.isLogin = true;
 				state.isAdmin = true;
+			}
+		},
+		logoutAdmin: async (state) => {
+			const cekLogout = await authservice.logoutAdmin();
+			if (cekLogout.status === 200) {
+				state.isAdmin = false;
+			}
+		},
+		logoutUser: async (state) => {
+			const cekLogout = await authservice.logoutUser();
+			if (cekLogout.status === 200) {
+				state.isUser = false;
 			}
 		},
 	},
 });
 
-export const { cekUser, cekAdmin } = authSlice.actions;
+export const { user, cekAdmin } = authSlice.actions;
 
 export default authSlice.reducer;
