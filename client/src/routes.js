c01d1ea3d802ca00a227727974d66fc5a57092f9
florensius daniel josef <capricondaniel@gmail.com>
2022-10-12 19:45:12 +0700

    resolve merging

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import HomePage from "./screens/home";
 import ProductDetailPage from "./screens/product-detail";
 import ProfilePage from "./screens/profile";
@@ -9,80 +8,18 @@ import Admin from "./screens/admin";
 import CartPage from "./screens/cart";
 import OrderPage from "./screens/order";
 import ProductListPage from "./screens/product-list";
-import { API } from "./configs/config";
 
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-=======
-import HomePage from "./pages/home";
-import ProductDetailPage from "./pages/product-detail";
-import ProfilePage from "./pages/profile";
-import LoginUser from "./pages/login/LoginUser";
-import LoginAdmin from "./pages/login-admin";
-import RegisterPage from "./pages/register";
-import Admin from "./pages/admin";
-import CartPage from "./pages/cart";
-import OrderPage from "./pages/order";
-import ProductListPage from "./pages/product-list";
-import { useAuth, useAuthAdmin } from "./hooks/useAuth";
-
-import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import { useEffect } from "react";
->>>>>>> origin/main
->>>>>>> origin
 
 const ProtectedRouteNonAuth = ({ children }) => {
-<<<<<<< HEAD
-  const navigate = useNavigate();
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    const cekUser = () => {
-      console.log("testing 2");
-      const verify = fetch(API + "/users/verify", {
-        method: "GET",
-        redirect: "follow",
-        credentials: "include",
-      });
-      console.log("verify", verify);
-
-      verify
-        .then((res) => {
-          console.log("res", res);
-          if (res.status === 200) {
-            navigate("/", { replace: true });
-          }
-        })
-        .catch((err) => {
-          console.log("error verify user = ", err);
-        });
-    };
-    cekUser();
-  }, []);
-=======
 	const navigate = useNavigate();
 
-<<<<<<< HEAD
 	const isUser = useAuth();
 
 	if (isUser === true) {
 		navigate("/profile", { replace: true });
 	}
-=======
-			if (verify.status === 200) {
-				navigate("/", { replace: true });
-			}
-		};
-		cekUser();
-	});
->>>>>>> origin/main
->>>>>>> origin
-
-  console.log("testing 1");
-  //   return children;
-  return children;
+	return children;
 };
 
 const ProtectedRouteAuth = ({ children }) => {
@@ -90,14 +27,10 @@ const ProtectedRouteAuth = ({ children }) => {
 
 	const isUser = useAuth();
 
-<<<<<<< HEAD
 	if (isUser === false) {
 		navigate("/login", { replace: true });
 	}
 	return children;
-=======
-  return children;
->>>>>>> origin
 };
 
 const ProtectedRouteAdmin = ({ children }) => {
@@ -122,78 +55,78 @@ const ProtectedRouteNonAuthAdmin = ({ children }) => {
 	return children;
 };
 const routes = [
-  {
-    path: "/",
-    page: <HomePage />,
-  },
-  {
-    path: "product/:product_id",
-    page: <ProductDetailPage />,
-  },
-  {
-    path: "product/category/:categoryId",
-    page: <ProductListPage />,
-  },
-  {
-    path: "profile",
-    page: (
-      <ProtectedRouteAuth>
-        <ProfilePage />
-      </ProtectedRouteAuth>
-    ),
-  },
-  {
-    path: "login",
-    page: (
-      <ProtectedRouteNonAuth>
-        <LoginUser />
-      </ProtectedRouteNonAuth>
-    ),
-  },
-  {
-    path: "register",
-    page: (
-      <ProtectedRouteNonAuth>
-        <RegisterPage />
-      </ProtectedRouteNonAuth>
-    ),
-  },
-  {
-    path: "admin/login",
-    page: (
-      <ProtectedRouteNonAuthAdmin>
-        <LoginAdmin />
-      </ProtectedRouteNonAuthAdmin>
-    ),
-  },
-  {
-    path: "/cart",
-    page: (
-      <ProtectedRouteAuth>
-        <CartPage />
-      </ProtectedRouteAuth>
-    ),
-  },
-  {
-    path: "/order/:orderId",
-    page: (
-      <ProtectedRouteAuth>
-        <OrderPage />
-      </ProtectedRouteAuth>
-    ),
-  },
-  {
-    path: "/admin",
-    page: (
-      <ProtectedRouteAdmin>
-        <Admin />
-      </ProtectedRouteAdmin>
-    ),
-  },
-  // {
-  // 	path: "/test",
-  // 	page: <Test />,
-  // },
+	{
+		path: "/",
+		page: <HomePage />,
+	},
+	{
+		path: "product/:product_id",
+		page: <ProductDetailPage />,
+	},
+	{
+		path: "product/category/:categoryId",
+		page: <ProductListPage />,
+	},
+	{
+		path: "profile",
+		page: (
+			<ProtectedRouteAuth>
+				<ProfilePage />
+			</ProtectedRouteAuth>
+		),
+	},
+	{
+		path: "login",
+		page: (
+			<ProtectedRouteNonAuth>
+				<LoginUser />
+			</ProtectedRouteNonAuth>
+		),
+	},
+	{
+		path: "register",
+		page: (
+			<ProtectedRouteNonAuth>
+				<RegisterPage />
+			</ProtectedRouteNonAuth>
+		),
+	},
+	{
+		path: "admin/login",
+		page: (
+			<ProtectedRouteNonAuthAdmin>
+				<LoginAdmin />
+			</ProtectedRouteNonAuthAdmin>
+		),
+	},
+	{
+		path: "/cart",
+		page: (
+			<ProtectedRouteAuth>
+				<CartPage />
+			</ProtectedRouteAuth>
+		),
+	},
+	{
+		path: "/order/:orderId",
+		page: (
+			<ProtectedRouteAuth>
+				<OrderPage />
+			</ProtectedRouteAuth>
+		),
+	},
+	{
+		path: "/admin",
+		page: (
+			<ProtectedRouteAdmin>
+				<Admin />
+			</ProtectedRouteAdmin>
+		),
+	},
+	// {
+	// 	path: "/test",
+	// 	page: <Test />,
+	// },
 ];
 
 export default routes;
