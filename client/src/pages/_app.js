fe8ac9820e1337ee6a8b01ea2cfaeae2f764e6e6
florensius daniel josef <capricondaniel@gmail.com>
2022-10-14 21:27:51 +0700

    perbaiki sedikit

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../index.css";
 import "../App.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
 	return (
